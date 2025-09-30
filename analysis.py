d48896f4b555a20f7df88abc1310cea60a64bd2b
ananya923 <ananyajogalekar5@gmail.com>
2025-09-30 17:02:13 -0400

    analysis lint edit

--- analysis.py
+++ analysis.py
@@ -8,8 +8,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-import seaborn as sns
-import matplotlib.pyplot as plt
+
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
 
 def load_data(filepath: str) -> pd.DataFrame:
