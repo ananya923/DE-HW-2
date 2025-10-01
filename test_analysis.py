ae70f568319a3388628038b6bde6c93e527fdad8
ananya923 <ananyajogalekar5@gmail.com>
2025-09-30 20:33:28 -0400

    lint fix

--- test_analysis.py
+++ test_analysis.py
@@ -3,19 +3,11 @@
 # Test File
 
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
 
 # importing the functions we want to test from other files
 from analysis import (
     load_data,
     clean_data,
-    groupby_income,
-    corr_by_researchTime,
-    groupby_mediaInfluence,
-    groupby_purchaseIntent,
     explore_data,
     run_random_forest,
 )
